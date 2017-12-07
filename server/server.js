3d792c3f181932eb594cc5ea7bf61e316605fbc2
Guillermo Olivos <gcolivos@gmail.com>
2017-12-07 14:35:47 -0600

    moved mongoose connection to separate module

--- server/server.js
+++ server/server.js
@@ -1,30 +1,22 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var app = express();
 
-var mongoose = require('mongoose');
-
+var mongooseConnection = require('./modules/mongoose-connection');
 var game = require('./routes/game');
 
+var app = express();
 
 var port = 5000;
 
 app.use(express.static('server/public'));
+
+// Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+mongooseConnection.connect();
 
 app.use('/game', game);
 
-var databaseUrl = 'mongodb://localhost:27017/game_chest';
-mongoose.connect(databaseUrl);
-
-mongoose.connection.on('connected', function(){
-    console.log('mongoose connected to :', databaseUrl);
-})
-
-mongoose.connection.on('error', function(err){
-    console.log('mongoose connection error', err)
-})
 
 
 
