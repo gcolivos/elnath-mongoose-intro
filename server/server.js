4675cb4f5a41b7ef39896eec0dd5b8ecc1ef1359
Guillermo Olivos <gcolivos@gmail.com>
2017-12-07 14:22:04 -0600

    working get request

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
 var app = express();
+
+var mongoose = require('mongoose');
+
+var game = require('./routes/game');
+
 
 var port = 5000;
 
@@ -9,12 +13,20 @@ app.use(express.static('server/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-var databaseUrl = 'mongo://localhost:27017/games';
+app.use('/game', game);
+
+var databaseUrl = 'mongodb://localhost:27017/game_chest';
 mongoose.connect(databaseUrl);
 
-app.get('/games', function(req, res){
-    res.sendStatus(200);
+mongoose.connection.on('connected', function(){
+    console.log('mongoose connected to :', databaseUrl);
 })
+
+mongoose.connection.on('error', function(err){
+    console.log('mongoose connection error', err)
+})
+
+
 
 app.listen(port,function(){
     console.log('listening on port', port);
